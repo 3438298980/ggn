dbe5f0639ca75f31ec6b6a051185831f63c581d7
3438298980 <85310075+3438298980@users.noreply.github.com>
2022-12-21 18:14:19 +0800

    Update date_zero_bash.sh

--- date_zero_bash.sh
+++ date_zero_bash.sh
@@ -1,16 +1,5 @@
 cd /content
-rm -f datetrain.csv
-rm -f date.csv
-rm -f train.bin
-wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/libstdc++.so.6 
-wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/GGnn_train
-wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/GGnn
-wget -c https://github.com/3438298980/ggn/raw/main/train.zip
-unzip train.zip
-cp libstdc++.so.6 /usr/lib/x86_64-linux-gnu/
-mv GGnn_train datetrain.csv
-mv GGnn date.csv
-chmod 777 datetrain.csv
-chmod 777 date.csv
-chmod 777 train.bin
-./datetrain.csv --user '泽田小言' --password 'zly4444' --lc0name 'date.csv' 
+rm -rf ./fsf_client
+git clone https://github.com/xyztnecniV/fsf_client
+cd ./fsf_client
+python client.py --user xiaoyan --threads 2
